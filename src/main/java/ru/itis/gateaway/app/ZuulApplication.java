f6324bdba07f1f3efc17830d2653609959842afe
DE-Karpov <mecazor.lucky@gmail.com>
2020-05-27 16:40:06 +0300

    Redundant annotation and dependencies removed

--- src/main/java/ru/itis/gateaway/app/ZuulApplication.java
+++ src/main/java/ru/itis/gateaway/app/ZuulApplication.java
@@ -4,11 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableSwagger2
-@EnableZuulProxy
+
 @SpringBootApplication
+@EnableZuulProxy
 @EnableEurekaClient
 public class ZuulApplication {
     public static void main(String[] args) {
